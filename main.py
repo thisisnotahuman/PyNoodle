91c8d485b434d380703e6f81a014d93ed33e1f81
tx_wang <3427244340@qq.com>
2025-04-30 15:24:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def main():
         csv_path=processed_data_path,
         init_weights=refined_result['optimal_weights'],
         tickers=refined_result['tickers'],
-        risk_free_rate=risk_free
+        risk_free_rate=risk_free.mean()
     )
     opt_row = {
         "sharpe": opt["sharpe"],
